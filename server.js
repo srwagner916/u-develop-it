e742429371e637138c861838cee3562e4b82ae79
Shay Wagner <srwagner916@gmail.com>
2021-05-10 12:16:14 -0500

    confirmed connection with MySQL database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,10 +8,28 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySql username,
+    user: 'root',
+    // Your MySQL pasword
+    password: 'margaretthatcheris110%SEXY',
+    database: 'election',
+  },
+  console.log('Connected to the election database.')
+);
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+  console.log(rows);
+});
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
